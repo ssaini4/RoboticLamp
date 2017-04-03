7926536829ae53012dd300b666e4ddbaf09c9130
AsianFishy <james@macbook-pro.na.aarp.int>
2017-04-03 16:02:35 -0500

    Brightness Control finished, needs testing

--- LEDs/BrightnessControl.py
+++ LEDs/BrightnessControl.py
@@ -10,25 +10,59 @@ class LEDLevel:
 BEGIN_LED = 0
 END_LED = 63
 
+# NeoPixel derived Variable
 matrix = null
+
+# Saved Pixel Color to Imitate
 pixelColor = null
+
+# Current LEDLevel Class
 curLevel = null
+
+# Current Brightness Index, [0,8]
+curIndex = null
+
+# Array [0,8] containing LEDLevel Classes
 LEDLevelArray = []
+
+savedMatrixRGB = []
 
 def initializeArray:
 	for i in range (0, 9):
 		LEDLevelArray.append(LEDLevel(i))
 
-# Sets RGB Value to Black of all LEDs up to (but not including) level first LED
-def offLEDs(level):
-	for i in range(curLevel.firstLED, LEDLevelArray[level].firstLED):
+# Going down a brightness level, set higher level row to BLACK
+def downLevel:
+	for i in range(curLevel.firstLED, LEDLevelArray[curIndex-1].firstLED):
 		matrix.setPixelColor(i, Colors.BLACK)
+		curLevel = LEDLevelArray[curIndex-1]
+		curIndex = curLevel.myLevel
+		matrix.setBrightness(curLevel.brightnessLevel)
 		
 # 
-def onLEDS(level):
+def upLevel:
+	for i in range(LEDLevelArray[curIndex+1].firstLED, curLevel.firstLED):
+		matrix.setPixelColor(i, pixelColor)
+		curLevel = LEDLevelArray[curIndex+1]
+		curIndex = curLevel.myLevel
+		matrix.setBrightness(curLevel.brightnessLevel)
 
-# Lamp goes into control mode
-def controlMode():
+# Lamp enters brightness control mode, saves previous RGB Values beforehand
+def enterControlMode():
+	# Save RGB Values
+	for i in range (0,63):
+		savedMatrixRGB[i] = matrix.getPixelColor(i)
+	# Set Brightness Levels
+	for i in range (0, curLevel.firstLED):
+		matrix.setPixelColor(i, Colors.BLACK)
+	for i in range (curLevel.firstLED, 64):
+		matrix.setPixelColor(i, pixelColor)
+
+# Lamp exits brightness control mode, puts back RGB Values
+def exitControlMode():
+	# Returns Saved RGB Values
+	for i in range (0,63):
+		matrix.setPixelColor(i, savedMatrixRGB[i])
 
 #Initialize, gets color from last LED
 def __init__(matrix):
@@ -36,3 +70,4 @@ def __init__(matrix):
 	self.pixelColor = matrix.getPixelColor(END_LED)
 	initializeArray()
 	curLevel = LEDLevelArray[4]
+	curIndex = 4
